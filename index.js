7e30636aa6f792bca7e3f8f04c6e9af26d4b13e2
AssoDePicche <samuelprado730@gmail.com>
2025-01-22 23:46:09 -0300

    feat: index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+export const items = [
+        {
+          name: "CNPq",
+          href: "https://lattes.cnpq.br/0506034609919899",
+          brand: "fa-regular fa-address-card"
+        },
+        {
+          name: "Facebook",
+          href: "https://www.facebook.com/samuel.do.prado.rodrigues/",
+          brand: "fa-brands fa-facebook"
+        },
+        {
+          name: "GitHub",
+          href: "https://github.com/AssoDePicche",
+          brand: "fa-brands fa-github"
+        },
+        {
+          name: "Instagram",
+          href: "https://www.instagram.com/assodepicche/",
+          brand: "fa-brands fa-instagram"
+        },
+        {
+          name: "Letterboxd",
+          href: "https://letterboxd.com/assodepicche/",
+          brand: "fa-brands fa-letterboxd"
+        },
+        {
+          name: "LinkedIn",
+          href: "https://www.linkedin.com/in/samuel-do-prado-rodrigues/",
+          brand: "fa-brands fa-linkedin"
+        },
+        {
+          name: "ResearchGate",
+          href: "https://www.researchgate.net/profile/Samuel-Rodrigues-26",
+          brand: "fa-brands fa-researchgate",
+        },
+        {
+          name: "Spotify",
+          href: "https://open.spotify.com/user/6flhz983p72i174tjdvshpnhs?si=268ddd24d3ad4bec",
+          brand: "fa-brands fa-spotify",
+        },
+        {
+          name: "Steam",
+          href: "https://steamcommunity.com/id/assodepicche/",
+          brand: "fa-brands fa-steam",
+        },
+        {
+          name: "Twitter",
+          href: "https://twitter.com/samuel_do_prado",
+          brand: "fa-brands fa-x-twitter",
+        }
+      ];
